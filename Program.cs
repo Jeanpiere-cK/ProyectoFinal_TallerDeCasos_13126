eec7911f968c5252c780abe9fd7bb24ae18dc99a
PIER <PIER@LAPTOP-6HBV5K9U>
2025-05-18 16:58:41 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Taller_de_Casos_13126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
