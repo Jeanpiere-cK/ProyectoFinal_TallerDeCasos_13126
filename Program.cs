4620c37a1ee6d3834a0769da5ac69246b6e7c1e0
PIER <PIER@LAPTOP-6HBV5K9U>
2025-05-18 17:31:19 -0500

    Avance #1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProyectoFinal_Taller_de_Casos_13126
@@ -10,7 +11,16 @@ namespace ProyectoFinal_Taller_de_Casos_13126
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("QUE PISO DEL EDIFICIO DESEA MONITOREAR: ");
+            Random random = new Random();
+            timer = new Timer();
+            const int TEMPERATURA = 140;
+            const int HUMO = 100;
+            
+        }
+        static void ContraIncendio()
+        {
+            
         }
     }
 }
